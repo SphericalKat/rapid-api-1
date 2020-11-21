45f909d00d20a3180771f26b05a19fe8f72b4a41
SphericalKat <amolele@gmail.com>
2020-11-22 03:10:37 +0530

    refactor(models): add an expiry field to Orders
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- api/models.py
+++ api/models.py
@@ -1,5 +1,7 @@
 import uuid
+from datetime import datetime
 
+import django
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -17,6 +19,7 @@ class Order(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     items = models.ManyToManyField(OrderItem)
     status = models.CharField(max_length=10, default='p', choices=(('p', 'pending'), ('c', 'complete')))
+    expiry = models.DateTimeField(default=django.utils.timezone.now)
 
     def __str__(self):
         return str(self.owner.get_username())
